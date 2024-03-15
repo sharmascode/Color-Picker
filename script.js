8123b9c15bd417b7c61a99e269d91413f78d4394
Manish Sharma <123705876+sharmascode@users.noreply.github.com>
2024-03-15 17:34:42 +0530

    Create script.js
    So adding a Javascript file to make it fully functional.

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript functionality for the colour picker tool
+// You can use a library like Spectrum.js for the colour picker functionality
+
+// Function to copy colour code to clipboard
+document.getElementById('copyColorCodeBtn').addEventListener('click', function() {
+    // Code to copy colour code
+    // You can use document.execCommand('copy') or Clipboard API for this functionality
+});
